4f90cce514101a4b0c9ca7b498bb0e547a733ca1
r-ior <sadfred3@gmail.com>
2019-08-15 20:45:38 +0300

    Created angular components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RedirectComponent } from './redirect/redirect.component';
+import { MainComponent } from './main/main.component';
+import { AccountComponent } from './account/account.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { UrlComponent } from './url/url.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RedirectComponent,
+    MainComponent,
+    AccountComponent,
+    RegisterComponent,
+    LoginComponent,
+    UrlComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
