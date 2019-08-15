8bc385305f2b956cb80bc21cb3d972e09a718d5b
r-ior <sadfred3@gmail.com>
2019-08-16 00:00:25 +0300

    Angular app routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RedirectComponent } from './redirect/redirect.component';
+import { MainComponent } from './main/main.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'account',
+        component: AccountComponent,
+        data: { title: 'Account' },
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        data: { title: 'Login into your account' },
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        data: { title: 'Create your account' },
+    },
+    {
+        path: ':short',
+        component: RedirectComponent,
+    },
+    {
+        path: '',
+        component: MainComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
