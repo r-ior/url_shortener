1656be19380f33020472bf0594fce06bff5eba5c
r-ior <sadfred3@gmail.com>
2019-08-18 20:53:50 +0300

    Header and footer fixes

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,18 @@ import { UserService } from '../user/user.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   isAuthenticated() {
     return this.userService.isAuthenticated();
+  }
+
+  logOut(event):void {
+    event.preventDefault();
+    localStorage.removeItem('authToken');
+
+    this.router.navigate(['/login']);
+    // window.location.reload();
   }
 
   ngOnInit() {
